bca6e0fda0986f239bc08cc9a9204566a50d64f3
Anna Galkina <sandhani1704@yandex.ru>
2020-10-11 19:49:05 +0300

    исправлен метод в  routerNonexistent

--- routes/nonexistent.js
+++ routes/nonexistent.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('*', (req, res) => {
+router.all('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
